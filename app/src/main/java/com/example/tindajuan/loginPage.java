33689f797b954d4842c12f3f87c18e4d11716140
berdelaradote12 <berdelaranaidororoo@gmail.com>
2023-08-24 16:19:07 +0800

    Add initial resources

---
+++ app/src/main/java/com/example/tindajuan/loginPage.java
@@ -0,0 +1,14 @@
+package com.example.tindajuan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
